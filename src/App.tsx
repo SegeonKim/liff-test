4d080c19b88a96c02cf809ab14cd74f6bb43042c
SegeonKim <segeon215@naver.com>
2024-11-19 11:29:55 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {liff} from "@line/liff";
@@ -7,6 +7,7 @@ function App() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const liffId = urlParams.get('liffId') as string;
+  const [stripePaymentUrl, setStripePaymentUrl] = useState("");
 
   const openLiff = async (url: string) => {
     console.log(liffId)
@@ -15,6 +16,32 @@ function App() {
     });
     await liff.subWindow.open({url: url});
   };
+
+  const createStripeCheckout = async () => {
+    const response = await fetch('https://api.stripe.com/v1/checkout/sessions', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer sk_test_51OCwvZKewluFfHAS8RbHZzah0P3qPnLKtC8i7x2b34CNZjIDlYfjhpVE4YB9nSWKNndFOJZOYYJ74f1iiK9QzvCu00tDwXSacE',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        'success_url': encodeURI('https://dosi.world/payment-callback/success'),
+        'cancel_url': encodeURI('https://dosi.world/payment-callback/cancel'),
+        'line_items[0][price_data][currency]': 'krw',
+        'line_items[0][price_data][product_data][name]': 'Checkout Test',
+        'line_items[0][price_data][product_data][images][0]': 'https://obs.line-scdn.net/0hoPTqjcuxMH9pCCKFTQZPKDFbNgQbbzI3BjY0RhtNcB0fOTJVCS8nS09VKTAybSp6CSh8SS5jJS4ieTFWDQV-eCpoMhohOT5BMAV_SilvKjMiajx5Izw5YhBSZjM0/w1200',
+        'line_items[0][price_data][unit_amount]': '1100',
+        'line_items[0][quantity]': '1',
+        'mode': 'payment',
+      }),
+    });
+
+    const session = await response.json();
+    console.log(session);
+    setStripePaymentUrl(session.url)
+
+    window.open(session.url, '_blank');
+  }
 
   return (
     <div className="App">
@@ -30,10 +57,16 @@ function App() {
           Open Liff - Tall
         </button>
         <br/>
-        <button className="open-button" onClick={() => {
-          openLiff("https://liff.line.me/2005519827-pArMwjaw")
+        <button className="open-button" onClick={async () => {
+          await openLiff("https://liff.line.me/2005519827-pArMwjaw")
         }}>
           Open Liff - Compact
+        </button>
+        <br/>
+        <button className="open-button" onClick={async () => {
+          await createStripeCheckout();
+        }}>
+          Stripe Checkout
         </button>
         <br/>
         <a
@@ -56,12 +89,13 @@ function App() {
         <br/>
         <a
             className="App-link"
-            href={`https://checkout.stripe.com/c/pay/cs_test_a10UYvpEG0wkAPhTVn8R9Um2xhnuQACGFUvKfMGuRBgDIHmfKZ4HK026pN#fidkdWxOYHwnPyd1blpxYHZxWjA0SkZyc19OYHJpcENjTURWVHNuZ3M2V0txYGdIdnRTdWRfUmAzY0hJaUxWanRzd2tNcUhfX1U0N3BWaUpPNW9sNmxnSGRtbTNWaHJUU2FWd3ZudFw9QVFyNTVMPXxQS3JgdScpJ2N3amhWYHdzYHcnP3F3cGApJ2lkfGpwcVF8dWAnPyd2bGtiaWBabHFgaCcpJ2BrZGdpYFVpZGZgbWppYWB3dic%2FcXdwYHgl`}
+            href={`https://uit-bluefruit.line-apps-beta.com/ko-KR/1st_sale/sales/82650/`}
             target="_blank"
             rel="noopener noreferrer"
         >
-          Open STRIPE
+          DOSI Test
         </a>
+
       </header>
     </div>
   );
